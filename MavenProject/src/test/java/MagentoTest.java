c2ebd2dc3042afc5764e2f6a9218185e9841a3c8
varun <varun@192.168.1.4>
2018-11-28 15:51:41 +0530

    Chrome

--- MavenProject/src/test/java/MagentoTest.java
+++ MavenProject/src/test/java/MagentoTest.java
@@ -3,7 +3,6 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 /**
